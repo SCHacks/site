b45a997c4ff4e4a32a39e2a44a859dd4307c9034
Lachlan Campbell <lachlan.campbell@icloud.com>
2018-03-26 01:06:23 -0400

    Add /pilot page

---
+++ src/pages/pilot.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import { Heading, Link } from '@hackclub/design-system'
+import Header from 'components/Header'
+import Bar from 'components/Bar'
+import Columns from 'components/Columns'
+import Column from 'components/Column'
+
+const links = [
+  {
+    href: 'https://glitch.com',
+    children: 'Glitch'
+  },
+  {
+    href: 'https://glitch.com/website-starter-kit',
+    children: 'Glitch tutorials'
+  },
+  {
+    href: 'https://hackclub.com/challenge',
+    children: '🏅 Challenge'
+  },
+  {
+    href: 'https://goo.gl/forms/Mgsh1lswaqrtf0xw2',
+    children: 'Feeback form'
+  }
+]
+
+export default () => (
+  <div>
+    <Header />
+    <Bar />
+    <Heading.h2 f={4}>Links</Heading.h2>
+    <Columns>
+      {links.map((props, i) => (
+        <Column key={i}>
+          <Link {...props} />
+        </Column>
+      ))}
+    </Columns>
+  </div>
+)
