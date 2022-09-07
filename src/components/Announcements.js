5ce47a289e075fc9adddff26d4aee8a2e402bd36
jxz21 <42892513+jxz21@users.noreply.github.com>
2022-09-06 22:26:59 -0400

    Create Announcements.js

---
+++ src/components/Announcements.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { Heading, Text } from '@hackclub/design-system'
+import Sheet from './Sheet'
+import List from './List'
+import Underlined from './Underlined'
+import data from 'data'
+
+const Announcements = () => {
+  <Sheet id="announcements">
+    <Heading.h2 fontSize={5} mb={2}>
+      Announcements
+    </Heading.h2>
+    <List>
+      {data.announcements.map((announcement, i) => {
+        <li key={announcement.slug}>
+          <Underlined
+            href={announcement.link}
+            children={announcement.message}
+            target="_blank"
+          />
+        </li>
+      })}
+    </List>
+  </Sheet>
+}
+
+export default Announcements
